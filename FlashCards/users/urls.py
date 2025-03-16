67628fcc26b93994154516fec9eb5bea3dbdd9fd
127_Kusey_Ksenia <113189436+KseniaKusey@users.noreply.github.com>
2025-03-16 18:01:24 +0300

    FlashCards_1

---
+++ FlashCards/users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+import django.contrib.auth.urls
+
+
+urlpatterns = [
+    path('', include('django.contrib.auth.urls')),
+   
+]
--- FlashCards/users/views.py
+++ FlashCards/users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth import logout
+
+def logout_view(request):
+    logout(request)
+    return render(request,'logout.html,{}')
+
+    
